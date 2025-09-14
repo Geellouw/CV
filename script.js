599769a26a8a366abbdb2a7ff22fd875d9dd9c6b
Geellouw <willem.louw1@gmail.com>
2025-09-14 19:59:19 +0200

    Updated CV with 2024 and 2025 data.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+document.getElementById("years").innerHTML = parseInt(new Date().getFullYear()) - 2017;
+
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
   window.print();
+});
+
+const pageCounters = document.querySelectorAll(".page-counter");
+const totalPages = pageCounters.length;
+
+pageCounters.forEach((counter, index) => {
+    const currentPageSpan = counter.querySelector(".current-page");
+    const totalPagesSpan = counter.querySelector(".total-pages");
+
+    if (currentPageSpan && totalPagesSpan) {
+        currentPageSpan.textContent = index + 1;
+        totalPagesSpan.textContent = totalPages;
+    }
 });
